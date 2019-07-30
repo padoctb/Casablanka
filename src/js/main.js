daddbbde1f0c20c0a3e2b5e6af87e772b0a70f56
Dmitry <dima@memco.ru>
2019-07-30 21:42:09 +0300

    our work js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import Slideout from 'slideout'
+import $ from 'jquery'
 
 // MENU
 const slideout = new Slideout({
@@ -14,3 +15,31 @@ menuButton.addEventListener('click', function openMenu() {
   this.classList.toggle('active')
   slideout.toggle();
 });
+
+// OUR WORK IMAGES
+const workTypes = document.querySelector('.our-work__types');
+let activeType = workTypes.children[0]
+const images = document.querySelector('.our-work__images')
+
+workTypes.addEventListener('click', (e) => {
+  const workType = e.target
+  if (workType.getAttribute('data-type') && workType !== activeType) {
+    activeType.classList.remove('our-work__type-active')
+    workType.classList.add('our-work__type-active')
+    activeType = workType
+
+    const imagesArray = [...images.children]
+
+    imagesArray.forEach((type) => {
+      if (type.getAttribute('data-image-type') !== activeType.getAttribute('data-type')) {
+        $(type).hide(300)
+      }
+      if (type.getAttribute('data-image-type') === activeType.getAttribute('data-type')) {
+        $(type).show(500)
+      }
+      if (activeType.getAttribute('data-type') === 'all') {
+        $(type).show(500)
+      }
+    })
+  }
+})
